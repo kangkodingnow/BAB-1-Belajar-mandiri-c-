5388f754592494124b5a153f9b0dc1fd22849aef
kangkodingnow <farhandwi234@gmail.com>
2022-07-19 12:33:22 +0700

    Variabel_localandlocal

--- Variabel dan tipe data/main.cpp
+++ Variabel dan tipe data/main.cpp
@@ -1,32 +1,23 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
+int a;
+
+void increment(){
+    a++;
+}
+
+void decrement(){
+    a--;
+}
+
 int main(){
-    unsigned int a{20};
-    long b{1234};
-    unsigned long c{123456};
-    unsigned long long d{12345678};
-    unsigned int pin;
-    unsigned int jumlah{0};
-
-    do{
-        cout << "Masukkan PIN: ";
-        cin >> pin;
-        jumlah++;
-    } while (jumlah < 3 && pin != 123456U);
-
-    if(jumlah == 3){
-        cout << "\nAnda sudah salah 3x memasukkan PIN, Akun diblokir!" << endl;
-    }else{
-        cout << "\nSelamat, Anda berhasil login.";
-    }
-
-    cout << "\n" << endl;
-    cout << sizeof(a) << endl;
-    cout << b << endl;
-    cout << c << endl;
-    cout << d << endl;
+    a = 10;
+    cout << "Nilai a mula-mula: " << a << endl;
+    increment();
+    cout << "Setelah dinaikkan 1: " << a << endl;
+    decrement();
+    cout << "setelah diturunkan 1: " << a << endl;
 
     return 0;
 }
