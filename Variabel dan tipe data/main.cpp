dde553d30e7b4bebf79768160d02830560e3781b
kangkodingnow <farhandwi234@gmail.com>
2022-07-19 12:32:05 +0700

    tipe_char_ascii

--- Variabel dan tipe data/main.cpp
+++ Variabel dan tipe data/main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+//convert lower case menjadi upper case
 int main(){
-    int a, b, c;
-    unsigned int f;
-    f = 20U;
-    cout << f << endl;
-    cout << "Masukkan tiga bilangan bulat: ";
-    cin >> a >> b >> c;
-
-    cout << "Anda memasukkan bilangan: " << a << " " << b << " " << c << " " << endl;
+    // for (int i = 65; i <= 90; i++){
+    //     cout << i << " = \'" << (char)i << "\'\t\t" << i + 32 << " = \'" << (char)(i + 32) << "\'" << endl;
+    // }
+    // cout << "aku \' kamu" << endl;
+    string s, temp;
+    cout << "Masukkan teks: ";
+    getline(cin, s);
+    temp = "";
+    for (unsigned int i = 0; i <= s.size(); i++){
+        char c = s[i];
+        int ascii = (int)c;
+        if(ascii >= 97 && ascii <= 122){
+            temp += (char)(ascii - 32);
+        }else{
+            temp += c;
+        }
+    }
+    cout << s.size() << endl;
+    cout << "\nSebelum diubah: " << s << endl;
+    cout << "setelah diubah: " << temp << endl;
 
     return 0;
 }
