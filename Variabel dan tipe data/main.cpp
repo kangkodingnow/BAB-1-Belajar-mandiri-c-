b9702984f6a53627664015017d8ad8b0305bfa43
kangkodingnow <farhandwi234@gmail.com>
2022-07-19 13:15:40 +0700

    Enumerasi_2

--- Variabel dan tipe data/main.cpp
+++ Variabel dan tipe data/main.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 using namespace std;
 
+struct Titik{
+    int x;
+    int y;
+};
+
+enum Arah{
+        atas,
+        kanan,
+        bawah,
+        kiri
+};
+
+void geserTitik(Titik &koordinat, Arah arah, int nilai){
+    switch(arah){
+        case atas:
+            koordinat.y += nilai;
+            break;
+        case kanan:
+            koordinat.x += nilai;
+            break;
+        case bawah:
+            koordinat.y -= nilai;
+            break;
+        case kiri:
+            koordinat.x -= nilai;
+            break;
+    }
+}
+
 int main(){
-    enum MataAngin
-    {
-        Timur = 1,
-        Tenggara,
-        Selatan,
-        BaratDaya,
-        Barat,
-        BaratLaut,
-        Utara,
-        TimurLaut
-    };
-    cout << "Timur\t\t: " << Timur << endl;
-    cout << "Tenggara\t: " << Tenggara << endl;
-    cout << "Selatan\t\t: " << Selatan << endl;
-    cout << "BaratDaya\t: " << BaratDaya << endl;
-    cout << "Barat\t\t: " << Barat << endl;
-    cout << "Utara\t\t: " << Utara << endl;
-    cout << "TimurLaut\t: " << TimurLaut << endl;
+    Titik A;
+    A.x = 10;
+    A.y = 20;
+
+    cout << "Titik sebelum digeser: A(" << A.x << "," << A.y << ")";
+    geserTitik(A, kanan, 10);
+    geserTitik(A, atas, 2);
+
+    cout << "\nTitik setelah digeser: A(" << A.x << "," << A.y << ")" << endl;
 
     return 0;
 }
