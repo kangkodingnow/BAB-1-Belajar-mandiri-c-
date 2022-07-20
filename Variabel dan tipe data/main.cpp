b34a1e945772396b83e19c788009ec1a5b38883b
kangkodingnow <farhandwi234@gmail.com>
2022-07-20 16:17:37 +0700

    konversi int ke char

--- Variabel dan tipe data/main.cpp
+++ Variabel dan tipe data/main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
-#include <typeinfo>
 using namespace std;
-int main(){
-    auto x = 10;
-    auto y{10.0F};
-    auto z(10.0);
 
-    cout << "Tipe x: " << typeid(x).name() << endl;
-    cout << "Tipe y: " << typeid(y).name() << endl;
-    cout << "Tipe z: " << typeid(z).name() << endl;
-    cout << x << " " << y << " " << z << endl;
+int main(){
+    for (int i = 0; i < 26; i++){
+        char c;
+        c = (char)(i + 65);
+        cout << c << " ";
+    }
 
     return 0;
 }
