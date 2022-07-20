0b11e0a373c7e8aed099de3390af521c052c7364
kangkodingnow <farhandwi234@gmail.com>
2022-07-20 15:54:36 +0700

    latihan struct 1

--- Variabel dan tipe data/main.cpp
+++ Variabel dan tipe data/main.cpp
@@ -5,7 +5,6 @@ struct Titik{
     int x;
     int y;
 };
-
 enum class Arah
 {
     atas,
@@ -35,11 +34,11 @@ int main(){
     Titik A;
     A.x = 0;
     A.y = 0;
-
+    printf("Nilai x: %d\n", A.x);
     cout << "Titik Sebelum Digeser: A(" << A.x << "," << A.y << ")" << endl;
     geserTitik(A, Arah::kanan, 10);
     geserTitik(A, Arah::atas, 2);
     cout << "Titik Setelah Digeser: A(" << A.x << "," << A.y << ")" << endl;
-
+    printf("Nilai x: %d\n", A.x);
     return 0;
 }
