9b47eba6a45471e5583af0f445f55c682776d53e
kangkodingnow <farhandwi234@gmail.com>
2022-07-21 08:23:46 +0700

    Membuat login dengan do-while

--- Pengulangan/main.cpp
+++ Pengulangan/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
+const string USERNAME{"admin"};
+const string PASSWORD{"Abc123"};
+
 int main(){
-    int data[5] = {1000, 2000, 3000, 4000, 5000};
-    //gaya lama
-    for (int i = 0; i < 5; i++){
-        cout << data[i] << endl;
-    }
-    cout << endl;
-    //gaya baru
-    for(int x:data){
-        cout << x << endl;
-    }
+    string username, password;
+
+    do{
+        cout << "Username: ";
+        cin >> username;
+        cout << "Password: ";
+        cin >> password;
+        cout << endl;
+    } while (username.compare(USERNAME) != 0 || password.compare(PASSWORD) != 0);
+
+    cout << "Selamat datang, " << username << endl;
 
     return 0;
 }
