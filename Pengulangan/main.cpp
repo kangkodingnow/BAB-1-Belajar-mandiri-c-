506a9986ed9deb7a903c815ae3b467a495a43e1d
kangkodingnow <farhandwi234@gmail.com>
2022-07-21 08:59:09 +0700

    Searching dan break

--- Pengulangan/main.cpp
+++ Pengulangan/main.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    string data[4][3] = {{"ATK01", "Spidol", "9000"}, {"ATK02", "Pensil", "6000"}, {"ATK03", "Stabilo", "11000"}, {"ATK04", "Tinta", "23000"}};
-    for (int i = 0; i < 4; i++){
-        cout << i + 1 << ". ";
-        for (int j = 0; j < 3; j++){
-            cout << data[i][j];
-            if(j < 2){
-                cout << ", ";
-            }
+
+void display(int *data){
+    cout << "DATA = ";
+    for (int i = 0; i < 10; i++){
+        cout << data[i];
+        if(i < 9){
+            cout << ", ";
         }
-        cout << endl;
+    }
+    cout << endl;
+}
+int main(){
+    int data[10] = {10, 13, 15, 19, 17, 16, 11, 17, 14, 12};
+    int x;
+    bool ketemu;
+    ketemu = 0;
+    display(data);
+    cout << "Masukkan nilai yang dicari: ";
+    cin >> x;
+    for (int i = 0; i < 10; i++){
+        if(data[i] == x){
+            cout << "data ditemukan pada index-" << i << endl;
+            ketemu = 1;
+            break;
+        }
+    }
+
+    if(ketemu==0){
+        cout << "Data tidak ditemukan di index manapun" << endl;
     }
 
     return 0;
 }
-
