d1a34f09520a04ba541a52db0d2b154f0e17ce3b
kangkodingnow <farhandwi234@gmail.com>
2022-07-21 20:11:12 +0700

    argc & argv

--- Dasar_dasar C++/main.cpp
+++ Dasar_dasar C++/main.cpp
@@ -1,38 +1,12 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-void print(string s, char end = '\n'){
-    cout << "std::string" << endl;
-    cout << s << endl;
-}
-
-void print(double d, char end = '\n'){
-    cout << "\ndouble" << endl;
-    cout << d << endl;
-}
-
-void print(float f, char end = '\n'){
-    cout << "\nFloat" << endl;
-    cout << f << endl;
-}
-
-void print(long l, char end = '\n'){
-    cout << "\nLong" << endl;
-    cout << l << endl;
-} 
-
-void print(int i, char end = '\n'){
-    cout << "\nint" << endl;
-    cout << i << endl;
-}
-
-int main(){
-    print("C++");
-    print(12.34);
-    print(45.6F);
-    print(1234567890L);
-    print(987);
+int main(int argc, char *argv[]){
+    cout << "jumlah argumen: " << argc << endl;
+    cout << "\nDaftar argumen: " << endl;
+    for (int i = 0; i < argc; i++){
+        cout << argv[i] << endl;
+    }
 
     return 0;
 }
