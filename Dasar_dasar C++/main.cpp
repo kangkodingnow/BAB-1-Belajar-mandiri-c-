f01118a44814a8907ef84619dc08afffa97827c6
kangkodingnow <farhandwi234@gmail.com>
2022-07-21 19:50:06 +0700

    fungsi overloading

--- Dasar_dasar C++/main.cpp
+++ Dasar_dasar C++/main.cpp
@@ -2,14 +2,37 @@
 #include <string>
 using namespace std;
 
-void print(string s, char end='\n'){
-    cout << s << end;
+void print(string s, char end = '\n'){
+    cout << "std::string" << endl;
+    cout << s << endl;
+}
+
+void print(double d, char end = '\n'){
+    cout << "\ndouble" << endl;
+    cout << d << endl;
+}
+
+void print(float f, char end = '\n'){
+    cout << "\nFloat" << endl;
+    cout << f << endl;
+}
+
+void print(long l, char end = '\n'){
+    cout << "\nLong" << endl;
+    cout << l << endl;
+} 
+
+void print(int i, char end = '\n'){
+    cout << "\nint" << endl;
+    cout << i << endl;
 }
 
 int main(){
-    print("Belajar");
-    print("Pemrograman", ' ');
     print("C++");
+    print(12.34);
+    print(45.6F);
+    print(1234567890L);
+    print(987);
 
     return 0;
 }
