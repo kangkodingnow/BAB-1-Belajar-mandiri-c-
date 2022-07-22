292a7ff8ea1703b54c10a8fab9130f246914cee3
kangkodingnow <farhandwi234@gmail.com>
2022-07-22 07:31:29 +0700

    tipe data pointer

--- Dasar_dasar C++/main.cpp
+++ Dasar_dasar C++/main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]){
-    cout << "jumlah argumen: " << argc << endl;
-    cout << "\nDaftar argumen: " << endl;
-    for (int i = 0; i < argc; i++){
-        cout << argv[i] << endl;
-    }
+int main(){
+    //mendeklarasikan variabel
+    int a{10};
+    double b{20.0};
+
+    //mendeklarasikan pointer
+    int *p1;
+    double *p2;
+
+    //pointer p1 menunjuk ke alamat a
+    p1 = &a;
+
+    //pointer p1 menunjuk ke alamat b
+    //p1 = &b;      salah
+
+    //pointer p2 menunjuk ke alamat b
+    p2 = &b;
+
+    cout << "a\t= " << a << endl;
+    cout << "&a\t= " << &a << endl;
+    cout << "*p1\t= " << *p1 << endl;
+    cout << "p1\t= " << p1 << endl;
+    cout << "&p1\t= " << &p1 << endl;
+
+    cout << "\nb\t= " << b << endl;
+    cout << "&b\t= " << &b << endl;
+    cout << "*p2\t= " << *p2 << endl;
+    cout << "p2\t= " << p2 << endl;
+    cout << "&p2\t= " << &p2 << endl;
 
     return 0;
 }
