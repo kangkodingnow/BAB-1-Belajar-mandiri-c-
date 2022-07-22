7f112f1e2c1a6306d1c282597e82471d12fb8e6e
kangkodingnow <farhandwi234@gmail.com>
2022-07-22 09:01:21 +0700

    pointer ke fungsi

--- Dasar_dasar C++/main.cpp
+++ Dasar_dasar C++/main.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 using namespace std;
 
+int cariPosisiNilai(int nilai, int *array, int n){
+    int indeks = -1;
+    for (int i = 0; i < n; i++){
+        if(array[i] == nilai){
+            indeks = i;
+            break;
+        }
+    }
+    return indeks;
+}
+
 int main(){
-    int a{5}, b{10};
-    int *const p = &a;
-    cout << "p selalu menunjuk alamat " << p << endl;
-    cout << "Sebelum diubah, *p = " << *p << endl;
-    cout << "alamat p = " << &p << endl;
-    *p = b;
-    cout << "Setelah diubah, *p = " << *p << endl;
-    cout << "alamat p = " << &p << endl;
+    int data[5] = {100, 200, 300, 400, 500};
+    int nilai;
+    int posisi;
+
+    //pointer ke fungsi
+    int (*cari)(int, int *, int) = &cariPosisiNilai;
+
+    cout << "Masukkan nilai yang dicari: ";
+    cin >> nilai;
+
+    posisi = cari(nilai, data, 5);
+    
+    if(posisi != -1){
+        cout << nilai << " ditemukan pada indeks ke-" << posisi << endl;
+    }else{
+        cout << nilai << " tidak ditemukan" << endl;
+    }
 
     return 0;
 }
