49146e47432e48e65502150e0334e1e315743bcb
kangkodingnow <farhandwi234@gmail.com>
2022-07-22 09:18:54 +0700

    fungsi dengan pointer

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void cetakArray(int *p, int n){
+    for (int i = 0; i < 5; i++){
+        cout << "p[" << i << "] = " << p[i] << endl;
+    }
+}
 int main(){
     int data[5] = {100, 200, 300, 400, 500};
     int *p;
     p = data; //tidak perlu ditulis p = &data
-    for (int i = 0; i < 5; i++){
-        cout << "p[" << i << "] = " << p[i] << endl;
-    }
-
+    void (*cetak)(int *, int n) = &cetakArray;
+    cetak(data, 5);
     return 0;
 }
