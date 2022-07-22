4bf3aa42f1fa2494bc7fe9f34ca7c159bbd62bae
kangkodingnow <farhandwi234@gmail.com>
2022-07-22 09:29:01 +0700

    aritmatika pointer 1

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -2,27 +2,14 @@
 using namespace std;
 
 int main(){
-    int data[5] = {100, 200, 300, 400, 500};
-    int *p;
-
-    p = data;
-    cout << "p menunjuk alamat elemen pertama, *p = " << *p;
-
-    p = p + 1;
-    cout << "\np menunjuk alamat elemen kedua, *p = " << *p;
-
-    p++;
-    cout << "\np menunjuk alamat elemen ketiga, *p = " << *p;
-
-    p++;
-    cout << "\np menunjuk alamat elemen keempat, *p = " << *p;
-
-    p++;
-    cout << "\np menunjuk alamat elemen kelima, *p = " << *p;
-
-    p--;
-    cout << "\np menunjuk alamat elemen keempat, *p = " << *p;
-
+    char data[4] = {'C', '+', '+', '\0'};
+    char *s;
+    s = data;
+    cout << *s << endl;
+    while(*s){
+        cout << *s;
+        s++;
+    }
     cout << endl;
 
     return 0;
