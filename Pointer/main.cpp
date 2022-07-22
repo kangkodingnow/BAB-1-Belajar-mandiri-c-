bc07b5fc181f3d50fbbb289210bc853d112f01ea
kangkodingnow <farhandwi234@gmail.com>
2022-07-22 13:37:13 +0700

    referensi 1

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 using namespace std;
 
+void ubahArray(int (&d)[5]){
+    for (int i = 0; i < 5; i++){
+        d[i] = d[i] * 2;
+    }
+}
+
+void cetakArray(int (&d)[5]){
+    for (int i = 0; i < 5; i++){
+        cout << d[i];
+        if(i < 4){
+            cout << ", ";
+        }
+    }
+    cout << endl;
+}
+
 int main(){
-    int a{10};
-    int &ra = a;
-
-    cout << "Sebelum a dan ra diubah" << endl;
-    cout << "a\t= " << a << endl;
-    cout << "ra\t= " << ra << endl;
-
-    //mengubah nilai a
-    a = 20;
-    cout << "Setelah a diubah" << endl;
-    cout << "a\t= " << a << endl;
-    cout << "ra\t= " << ra << endl;
-
-    //mengubah nilai ra
-    ra = 100;
-    cout << "Setelah ra diubah" << endl;
-    cout << "a\t= " << a << endl;
-    cout << "ra\t= " << ra << endl;
+    int data[5]{100, 200, 300, 400, 500};
+    cout << "Sebelum diubah: ";
+    cetakArray(data);
+    ubahArray(data);
+    cout << "Setelah diubah: ";
+    cetakArray(data);
 
     return 0;
 }
