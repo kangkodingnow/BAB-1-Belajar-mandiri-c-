d70b7c95287cc70d7dcc75e0e17fd70fceca31da
kangkodingnow <farhandwi234@gmail.com>
2022-07-22 09:35:31 +0700

    pointer to pointer

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -2,15 +2,24 @@
 using namespace std;
 
 int main(){
-    char data[4] = {'C', '+', '+', '\0'};
-    char *s;
-    s = data;
-    cout << *s << endl;
-    while(*s){
-        cout << *s;
-        s++;
-    }
-    cout << endl;
+    int a{9};
+    int *p1;
+    int **p2;
+
+    p1 = &a;
+    p2 = &p1;
+
+    cout << "a\t = " << a << endl;
+    cout << "&a\t = " << &a << endl;
+
+    cout << "\n*p1\t = " << *p1 << endl;
+    cout << "p1\t = " << p1 << endl;
+    cout << "&p1\t = " << &p1 << endl;
+
+    cout << "\n*p2\t = " << *p2 << endl;
+    cout << "**p2\t = " << **p2 << endl;
+    cout << "p2\t = " << p2 << endl;
+    cout << "&p2\t = " << *p2 << endl;
 
     return 0;
 }
