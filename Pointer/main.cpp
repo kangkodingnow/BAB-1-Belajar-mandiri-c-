ebe1bd777cb82dca170a93e4816c99c9cb77de70
kangkodingnow <farhandwi234@gmail.com>
2022-07-22 09:23:31 +0700

    aritmatika pointer

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void cetakArray(int *p, int n){
-    for (int i = 0; i < 5; i++){
-        cout << "p[" << i << "] = " << p[i] << endl;
-    }
-}
 int main(){
     int data[5] = {100, 200, 300, 400, 500};
     int *p;
-    p = data; //tidak perlu ditulis p = &data
-    void (*cetak)(int *, int n) = &cetakArray;
-    cetak(data, 5);
+
+    p = data;
+    cout << "p menunjuk alamat elemen pertama, *p = " << *p;
+
+    p = p + 1;
+    cout << "\np menunjuk alamat elemen kedua, *p = " << *p;
+
+    p++;
+    cout << "\np menunjuk alamat elemen ketiga, *p = " << *p;
+
+    p++;
+    cout << "\np menunjuk alamat elemen keempat, *p = " << *p;
+
+    p++;
+    cout << "\np menunjuk alamat elemen kelima, *p = " << *p;
+
+    p--;
+    cout << "\np menunjuk alamat elemen keempat, *p = " << *p;
+
+    cout << endl;
+
     return 0;
 }
