b8eb61de3a7a298ac56bb00e2b7cf731c09a472a
kangkodingnow <farhandwi234@gmail.com>
2022-07-30 12:48:56 +0700

    Akses file, Membaca data ke dalam file

--- I & O Gaya C/Main.cpp
+++ I & O Gaya C/Main.cpp
@@ -1,33 +1,29 @@
-#include <iostream>
 #include <cstdio>
 #include <cstdlib>
-
-using namespace std;
 
 int main(int argc, char const *argv[])
 {
     //mendeklarasikan pointer ke file
     FILE *fp;
 
-    //Membuka file
-    //MODE:
-    //mode "w" digunakan saat kita ingin menulis data ke dalam file. menciptkan file baru di dalam disk. jika file yang akan diakses sudah ada di dalam disk maka isi file tersebut akan ditimpa
-    //mode "a" jika kita ingin menambahkan data yang sudah ada sebelumnya
-    //mode 'r' digunakan untuk mode membaca file
-    fp = fopen("data.txt", "a");
+    //Mendeklarasikan variable untuk menampung data yang dibaca
+    char s[255];
+
+    //membuka file untuk penmabahan data
+    fp = fopen("data.txt", "r");
 
     if(!fp){
-        printf("SALAH: proses pembuatan file gagal\n");
-        exit(EXIT_FAILURE);
+        printf("SALAH: File tidak ditemukan\n");
+        exit(0);
     }
 
-    //menulis data ke dalam file
-    fputs("Pemrograman C++\n", fp);
-    fputs("Pemrograman Python\n", fp);
-    fputs("Pemrograman PHP\n", fp);
-    fputs("Pemrograman ruby\n", fp);
+    //Membaca seluruh data di dalam file
+    while(fgets(s, 255, fp)){
+        printf("%s", s);
+    }
 
+    //menutup file
     fclose(fp);
-
+   
     return 0;
 }
