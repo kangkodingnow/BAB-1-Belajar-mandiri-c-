8197cfe2193840d0eb4eb829a75860c9095681fe
kangkodingnow <farhandwi234@gmail.com>
2022-07-30 12:43:08 +0700

    Akses file, Menulis data ke dalam file

--- I & O Gaya C/Main.cpp
+++ I & O Gaya C/Main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    float a;
-    double b;
-    int c = 10;
-    char *d;
-    sprintf(d, "%d", c);
-    printf("Masukkan nilai a: ");
-    scanf("%f", &a);
-    printf("Masukkan nilai b: ");
-    scanf("%lf", &b);
+    //mendeklarasikan pointer ke file
+    FILE *fp;
 
-    printf("\nData yang dimasukkan: %f dan %lf\n", a, b);
+    //Membuka file
+    //MODE:
+    //mode "w" digunakan saat kita ingin menulis data ke dalam file. menciptkan file baru di dalam disk. jika file yang akan diakses sudah ada di dalam disk maka isi file tersebut akan ditimpa
+    //mode "a" jika kita ingin menambahkan data yang sudah ada sebelumnya
+    //mode 'r' digunakan untuk mode membaca file
+    fp = fopen("data.txt", "w");
+
+    if(!fp){
+        printf("SALAH: proses pembuatan file gagal\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //menulis data ke dalam file
+    fputs("Pemrograman C++\n", fp);
+    fputs("Pemrograman Python\n", fp);
+    fputs("Pemrograman PHP\n", fp);
+    fputs("Pemrograman ruby\n", fp);
+
+    fclose(fp);
+
     return 0;
 }
