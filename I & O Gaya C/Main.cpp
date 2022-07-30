8cf31cf5d2d60c1de27a7ea6a6457123c31e4a5f
kangkodingnow <farhandwi234@gmail.com>
2022-07-30 12:44:39 +0700

    Akses file, Menambah data ke dalam file

--- I & O Gaya C/Main.cpp
+++ I & O Gaya C/Main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
     //mode "w" digunakan saat kita ingin menulis data ke dalam file. menciptkan file baru di dalam disk. jika file yang akan diakses sudah ada di dalam disk maka isi file tersebut akan ditimpa
     //mode "a" jika kita ingin menambahkan data yang sudah ada sebelumnya
     //mode 'r' digunakan untuk mode membaca file
-    fp = fopen("data.txt", "w");
+    fp = fopen("data.txt", "a");
 
     if(!fp){
         printf("SALAH: proses pembuatan file gagal\n");
